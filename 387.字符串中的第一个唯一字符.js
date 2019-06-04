2aeff34a15186d96b02c3373c476e3b557b1b2ba
nvnvyezi <nvnvyezi@gmail.com>
2019-06-04 12:16:49 +0800

    feat 字符串中的第一个唯一字符

---
+++ 387.字符串中的第一个唯一字符.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=387 lang=javascript
+ *
+ * [387] 字符串中的第一个唯一字符
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+  let obj = {};
+  for (let i = 0; i < s.length; i++) {
+    if (obj[s[i]]) {
+      obj[s[i]]++;
+    } else {
+      obj[s[i]] = 1;
+    }
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (obj[s[i]] === 1) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+var firstUniqChar = function(s) {
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+      return i;
+    }
+  }
+  return -1;
+};
--- README.md
+++ README.md
@@ -151,3 +151,5 @@
 - [两整数之和](https://github.com/nvnvyezi/LeetCode/blob/master/371.%E4%B8%A4%E6%95%B4%E6%95%B0%E4%B9%8B%E5%92%8C.js)
 
 - [赎金信](https://github.com/nvnvyezi/LeetCode/blob/master/383.%E8%B5%8E%E9%87%91%E4%BF%A1.js)
+
+- [字符串中的第一个唯一字符](https://github.com/nvnvyezi/LeetCode/blob/master/387.%E5%AD%97%E7%AC%A6%E4%B8%B2%E4%B8%AD%E7%9A%84%E7%AC%AC%E4%B8%80%E4%B8%AA%E5%94%AF%E4%B8%80%E5%AD%97%E7%AC%A6.js)
